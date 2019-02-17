2064130bdd31106a6845d30a3eddbc33d6d6e85e
SumitKJ <sumitkj7.5@gmail.com>
2019-02-17 12:07:45 +0530

    commit 1.0

--- app/src/main/java/com/example/poiuytrewq/MainActivity.java
+++ app/src/main/java/com/example/poiuytrewq/MainActivity.java
@@ -28,8 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Todo switch to the News Feed
             }
         });
 
